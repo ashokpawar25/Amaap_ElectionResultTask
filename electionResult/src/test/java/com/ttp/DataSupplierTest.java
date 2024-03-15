f6696e5bb8f3aa1981330d49de2106e015d02668
ashokpawar25 <ashokpawar25052001@gmail.com>
2024-03-15 21:40:09 +0530

    Setup Ready

---
+++ electionResult/src/test/java/com/ttp/DataSupplierTest.java
@@ -0,0 +1,12 @@
+package com.ttp;
+
+import org.junit.jupiter.api.Test;
+
+public class DataSupplierTest
+{
+    @Test
+    void shouldAbleToGetDataFromDataSupplier()
+    {
+        DataSupplier dataSupplier = new DataSupplier();
+    }
+}
