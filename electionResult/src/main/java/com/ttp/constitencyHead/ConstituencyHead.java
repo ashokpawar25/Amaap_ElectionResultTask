b80d041f950d3274687a52696f42a197305b461e
ashokpawar25 <ashokpawar25052001@gmail.com>
2024-03-16 21:32:26 +0530

    result for single constituency

---
+++ electionResult/src/main/java/com/ttp/constitencyHead/ConstituencyHead.java
@@ -0,0 +1,39 @@
+package com.ttp.constitencyHead;
+
+import com.ttp.dataSupplier.dataSupplier.DataSupplier;
+import com.ttp.partyCodeMapper.PartCodeMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConstituencyHead {
+    PartCodeMapper partCodeMapper;
+    DataSupplier dataSupplier;
+    public ConstituencyHead(PartCodeMapper partCodeMapper, DataSupplier dataSupplier)
+    {
+        this.partCodeMapper=partCodeMapper;
+        this.dataSupplier=dataSupplier;
+    }
+
+    public Map<String, Map<String, Integer>> analyzeResult()
+    {
+        Map<String, Map<String, Integer>> result = dataSupplier.getResult();
+        Map<String, Map<String, Integer>> constituncyWinner = new HashMap<>();
+//        <Banglore, <INC, 11014>,<BJP, 17803>,<CPI, 4923>,<NCP, 2069>>
+        for(Map.Entry<String, Map<String, Integer>> constituency:result.entrySet()) {
+            Map<String, Integer> winningPartyData = new HashMap<>();
+            int winnerPartyVotes = 0;
+            String winningPartyName = "";
+            for (Map.Entry<String, Integer> entry : constituency.getValue().entrySet()) {
+
+                if (entry.getValue() > winnerPartyVotes) {
+                    winnerPartyVotes = entry.getValue();
+                    winningPartyName= entry.getKey();
+                }
+            }
+            winningPartyData.put(winningPartyName, winnerPartyVotes);
+            constituncyWinner.put( constituency.getKey(), winningPartyData);
+        }
+        return constituncyWinner;
+    }
+}
--- electionResult/src/test/java/com/ttp/constituencyHead/ConstituencyHeadTest.java
+++ electionResult/src/test/java/com/ttp/constituencyHead/ConstituencyHeadTest.java
@@ -1,14 +1,38 @@
 package com.ttp.constituencyHead;
 
+import com.ttp.constitencyHead.ConstituencyHead;
+import com.ttp.dataSupplier.dataSupplier.DataSupplier;
+import com.ttp.partyCodeMapper.PartCodeMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ConstituencyHeadTest
-{
-    @Test
-    void shouldAbleToAnalyzeDataAndDeclareWinner()
-    {
-        //Arrange
+import java.util.HashMap;
+import java.util.Map;
 
-//        ConstituencyHead constituencyHead ;
+public class ConstituencyHeadTest {
+    @Test
+    void shouldAbleToAnalyzeDataAndDeclareWinner() {
+        //Arrange
+        PartCodeMapper partCodeMapper = new PartCodeMapper();
+
+        Map<String, Map<String, Integer>> result = new HashMap<>();
+        Map<String, Integer> bangloreResult = new HashMap<>();
+        bangloreResult.put("INC", 11014);
+        bangloreResult.put("BJP", 17803);
+        bangloreResult.put("CPI", 4923);
+        bangloreResult.put("NCP", 2069);
+        result.put("Banglore", bangloreResult);
+        DataSupplier dataSupplier = new DataSupplier(result);
+        Map<String, Map<String, Integer>> expectedWinner = new HashMap<>();
+        Map<String, Integer> winnerData = new HashMap<>();
+        winnerData.put("BJP", 17803);
+        expectedWinner.put("Banglore", winnerData);
+
+        //Act
+        ConstituencyHead constituencyHead = new ConstituencyHead(partCodeMapper, dataSupplier);
+        Map<String, Map<String, Integer>> actualWinner = constituencyHead.analyzeResult();
+
+        //Assert
+        Assertions.assertEquals(expectedWinner, actualWinner);
     }
 }
